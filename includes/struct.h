247323fd88877a030c1ea23027689ada42c1c337
Simon Thomas <simon.thomas.tux@gmail.com>
2025-01-20 11:18:30 +0100

    header minor fix

--- includes/struct.h
+++ includes/struct.h
@@ -6,9 +6,12 @@
 /*   By: sithomas <sithomas@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 14:55:44 by sithomas          #+#    #+#             */
-/*   Updated: 2025/01/15 14:55:58 by sithomas         ###   ########.fr       */
+/*   Updated: 2025/01/20 11:18:05 by sithomas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef STRUCT_H
+# define STRUCT_H
 
 typedef struct s_lst_stack
 {
@@ -16,3 +19,5 @@ typedef struct s_lst_stack
 	unsigned int		pos;
 	struct s_lst_stack	*next;
 }				t_lst_stack;
+
+#endif 
